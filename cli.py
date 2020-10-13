cf0e99e1d6d81ebb34a15db49dcc4fd318b2b7a0
tabira2382 <tabira2382@gmail.com>
2020-10-13 23:48:29 +0900

    コードの設計を考えた（役割と名前）

---
+++ cli.py
@@ -0,0 +1,16 @@
+def main():
+    # ユーザーから入力（郵便番号）を受け取る
+    zipcode = input('郵便番号　> ')
+
+    # 受け取った郵便番号を使ってWebAPIを呼び出して、地名を取得する
+    # ex: 8480046 -> 佐賀県伊万里市伊万里町乙
+    address = my_zipcloud.fetch_address(zipcode)
+
+    # 取得した地名を最終出力の形式に整形する
+    output = my_formatter.simple_formatter(address)
+
+    #　最終的な出力内容を出力する
+    print(output)
+
+if __name__ == '__main__':
+    main()
