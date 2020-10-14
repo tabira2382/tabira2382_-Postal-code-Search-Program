462e7bc9e3f1c18c14907b04eaa7a2f207a55eea
tabira2382 <tabira2382@gmail.com>
2020-10-14 22:55:42 +0900

    fetch_addressの実装完了

--- my_formatter.py
+++ my_formatter.py
@@ -1,3 +1,5 @@
 def simple_formatter(zipcode: str, address: str) -> str:
     # TODO: WebAPIを呼び出す予定
     return f'{zipcode}は「{address}」です'
+
+
--- my_zipcloud.py
+++ my_zipcloud.py
@@ -1,3 +1,26 @@
+import requests
+
+
 def fetch_address(zipcode: str) -> str:
     # TODO: WebAPIを呼び出す予定
-    return '佐賀県伊万里市伊万里町乙'
+
+    url = f'https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
+    response = requests.get (url)
+
+    response_json = response.json ()
+    address1 = response_json['results'][0]['address1']
+    address2 = response_json['results'][0]['address2']
+    address3 = response_json['results'][0]['address3']
+
+    return f'{address1}{address2}{address3}'
+
+
+def main():
+    zipcode = '8541123'
+    address = fetch_address (zipcode)
+
+    print (address)
+
+
+if __name__ == '__main__':
+    main ()
